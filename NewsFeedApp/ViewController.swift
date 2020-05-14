d05e570403cc1a32de3d8242cc793632b27a8ad2
David Marjanović <david.marjanovic@factory.hr>
2020-05-14 12:03:18 +0200

    Initial Commit

---
+++ NewsFeedApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsFeedApp
+//
+//  Created by David Marjanović on 14/05/2020.
+//  Copyright © 2020 David Marjanović. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
